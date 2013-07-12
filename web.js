13f1627157d09c52aead04d897bc7a4ca2b804da
Deivide Oliveira <deivide.s.oliveira@gmail.com>
2013-07-12 23:08:18 +0000

    Added Buffer to index.html - First test

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs  = require('fs');
+var inputfile = fs.readFileSync('index.html');
+var buf = new Buffer(inputfile);
+var out = buf.toString(0,buffer.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(inputfile);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
