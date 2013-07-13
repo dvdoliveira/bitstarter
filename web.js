f43e0023238032f7fca3b0a0f0708bf314aa78e3
Deivide Oliveira <deivide.s.oliveira@gmail.com>
2013-07-13 01:08:54 +0000

    +1.5

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-var fs  = require('fs');
-//var inputfile = fs.readFileSync('index.html');
-//var buf = new Buffer(inputfile);
-//var out = buf.toString(0,buffer.length);
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-//  var inputfile = fs.readfileSync('index.html');
-//  var buf = new Buffer(inputfile);
-//  var out = buf.toString(0;buffer.length);
-  response.send(fs.readFileSync("index.html", "utf8"));
+var inputfile = fs.readfileSync("index.html", "utf8");
+//var buf = new Buffer(inputfile);
+//var out = buf.toString(0;buffer.length);
+  response.send(inputfile);
 });
 
 Var port = process.env.PORT || 5000;
