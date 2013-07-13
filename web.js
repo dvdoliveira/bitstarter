725cbbdaa7635cb310c11d13cf81cb152d6b69b8
Deivide Oliveira <deivide.s.oliveira@gmail.com>
2013-07-13 01:05:19 +0000

    +1.4

--- web.js
+++ web.js
@@ -10,11 +10,12 @@ app.get('/', function(request, response) {
 //  var inputfile = fs.readfileSync('index.html');
 //  var buf = new Buffer(inputfile);
 //  var out = buf.toString(0;buffer.length);
-  response.send(fs.readFileSync("index.html", 'utf8'));
+  response.send(fs.readFileSync("index.html", "utf8"));
 });
 
 Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
 
