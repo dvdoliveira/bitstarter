fc5d9833efb09254867309465fd5dc955a6da780
Deivide Oliveira <deivide.s.oliveira@gmail.com>
2013-07-17 05:40:26 +0000

    Correcting bugs on grader.js

--- grader.js
+++ grader.js
@@ -26,12 +26,17 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)); {
+    if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
-	proces.exit(1);
+	process.exit(1);
     }
     return instr;
 };
+
+var cheerioHtmlFile = function(htmlfile) { 
+    return cheerio.load(fs.readFileSync(htmlfile));
+}; 
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -41,10 +46,9 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks){
-	var present = $(checks[ii].length > 0;
-	out[checks[ii]] = present;
-	
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;	
     }
 	return out;
 };
@@ -55,11 +59,11 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKFILE_DEFAULT)
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringfy(checkJson, null, 4);
+    var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
